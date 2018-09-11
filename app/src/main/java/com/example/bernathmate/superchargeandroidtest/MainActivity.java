a037d317c6cbb7d093ec5b11f713a1d75b83c6a8
Bernáth Máté <bernath.mate.new@gmail.com>
2018-09-11 12:11:36 +0200

    The MainActivity class contains two fields (API_KEY as a String, theMovieDBService instance).

--- app/src/main/java/com/example/bernathmate/superchargeandroidtest/MainActivity.java
+++ app/src/main/java/com/example/bernathmate/superchargeandroidtest/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.bernathmate.superchargeandroidtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bernathmate.superchargeandroidtest.service.TheMovieDBService;
+import com.example.bernathmate.superchargeandroidtest.service.TheMovieDBServiceImpl;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY = "43a7ea280d085bd0376e108680615c7f";
+
+    public TheMovieDBService theMovieDBService = TheMovieDBServiceImpl.createImplementationToTMDBS();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
